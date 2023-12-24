914119d8c96243d3d30bdb92262c7e6029b681fa
duckiendoan <kiendd.hrt@gmail.com>
2023-12-24 09:40:34 +0700

    Test new model

--- models.py
+++ models.py
@@ -25,3 +25,32 @@ class JointModel(nn.Module):
         x = torch.cat([x1, x2], dim=1)
         out = self.fc(x)
         return out
+    
+class JointModelv2(nn.Module):
+    def __init__(self, image_model, text_model, num_classes=18):
+        super(JointModel, self).__init__()
+        self.image_model = image_model
+        del self.image_model.fc
+        self.text_model = text_model
+        self.f_image = nn.Linear(2048, 768)
+        # self.f_text = nn.Linear(text_model.config.hidden_size, 256)
+        self.fc = nn.Sequential(
+            # nn.Dropout(p=0.2),
+            nn.Linear(768 * 2, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.Dropout(p=0.3),
+            nn.ReLU(),
+            nn.Linear(256, num_classes),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input_ids, attention_mask, image):
+        img_feature = self.image_model(image)
+        text_feature = self.text_model(input_ids, attention_mask).last_hidden_state
+        x1 = self.f_image(img_feature)
+        # x2 = self.f_text(text_feature[:, 0, :])
+        x2 = text_feature[:, 0, :]
+        x = torch.cat([x1, x2], dim=1)
+        out = self.fc(x)
+        return out
